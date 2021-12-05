3315dae26dc91181a9749e1ce97743011b9a1071
theosABS <ferien@apartmomente.ch>
2021-12-05 15:46:50 +0100

    Basel Jüdisches Museum
    Beitrag erstellen; leider sehe ich den Beitrag in Ort nicht, um zu kontrollieren, ob ich alles richtig gemacht habe.

---
+++ src/app/basel-juedisches-museum/basel-juedisches-museum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basel-juedisches-museum',
+  templateUrl: './basel-juedisches-museum.component.html',
+  styleUrls: ['./basel-juedisches-museum.component.scss']
+})
+export class BaselJuedischesMuseumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

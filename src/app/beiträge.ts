45275a7f7c2944c73cb2b33b4baa7ff6e4c8715f
theosABS <ferien@apartmomente.ch>
2022-10-07 17:05:56 +0200

    aktualisiert

--- src/app/beiträge.ts
+++ src/app/beiträge.ts
@@ -27,11 +27,7 @@ export const Beiträge = {
     kanton: Kantone.Waadt,
   },
   
-<<<<<<< Updated upstream
  '6 Bern, Matte: Ländtetor': {
-=======
-   '6 Bern, Matte: Ländtetor': {
->>>>>>> Stashed changes
     karusselltitel: 'Bern, Matte: Ländtetor',
     verzeichnispfad: 'mattelaendtetor',
     beitragtitel: 'Ländtetor',
@@ -42,10 +38,6 @@ export const Beiträge = {
     markierungen: [[46.94897, 7.45807]],
     kanton: Kantone.Trouvaillen,
   },
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   
   '5 Kirche Einigen "Unser täglich Brot ..."': {
     karusselltitel: 'Kirche Einigen: "Unser täglich Brot ..."',
@@ -71,13 +63,9 @@ export const Beiträge = {
     kanton: Kantone.Trouvaillen,
   },
 
- 
 
-<<<<<<< Updated upstream
 */
 
-=======
->>>>>>> Stashed changes
   'Basel Galluspforte': {
     karusselltitel: 'Die Galluspforte am Basler Münster',
     verzeichnispfad: 'Die Galluspforte am Basler Münster',

0bd22f8bba83958d1a981a882856a9a6e7e80ef9
theosABS <ferien@apartmomente.ch>
2024-05-30 11:06:42 +0200

    Beitrag Herz Jesu Wiedikon erstellt

--- src/app/beiträge/zuerich-herzjesuwiedikon/zuerich-herzjesuwiedikon.component.ts
+++ src/app/beiträge/zuerich-herzjesuwiedikon/zuerich-herzjesuwiedikon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/components/services/data-service.service';
+import { MapService } from 'src/app/components/services/map.service';
+import { MarkerService } from 'src/app/components/services/marker.service';
 
 @Component({
   selector: 'app-zuerich-herzjesuwiedikon',
@@ -6,4 +9,74 @@ import { Component } from '@angular/core';
   templateUrl: './zuerich-herzjesuwiedikon.component.html',
   styleUrl: './zuerich-herzjesuwiedikon.component.scss',
 })
-export class ZuerichHerzjesuwiedikonComponent {}
+export class ZuerichHerzjesuwiedikonComponent {
+  ngOnInit(): void {
+    this.assign(
+      'Silke Weinig',
+      'Mitten in Zürich - wo man seine Spiritualität kunstvoll entfalten kann'
+    );
+    this.createIds();
+  }
+
+  coordinates: any;
+
+  ids: string[] = [];
+
+  maps: L.Map[] = [];
+
+  author: any;
+
+  title!: string;
+
+  pathname!: string;
+
+  date!: string;
+
+  constructor(
+    protected markerService: MarkerService,
+    protected dataService: DataServiceService,
+    protected mapService: MapService
+  ) {}
+
+  assign(author: string, title: string) {
+    const beitrag = this.dataService.getBeitrag(author, title);
+    this.author = this.dataService.getAuthor(author, title);
+
+    this.date = this.dataService.getDate(author, title)!;
+    this.coordinates = beitrag!.markers;
+    this.title = beitrag!.fulltitle;
+    this.pathname = beitrag!.pathname;
+
+    this.replaceWhiteSpace();
+  }
+
+  replaceWhiteSpace() {
+    for (let i = 0; i < this.author.beitrag.length; i += 1) {
+      this.author.pathname[i] = this.author.pathname[i]
+        .replace(/\s+/g, '-')
+        .toLowerCase();
+    }
+  }
+  createIds() {
+    for (let i = 0; i < this.coordinates.length; i += 1) {
+      this.ids.push(`${this.title}${i}`);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.ids.length > 0) {
+      for (let i = 0; i < this.coordinates.length; i += 1) {
+        this.maps.push(
+          this.mapService.initMap(
+            this.coordinates[i],
+            16,
+            true,
+            8.5,
+            `${this.title}${i}`
+          )
+        );
+        this.markerService.makeMarkers(this.maps[i], this.coordinates[i]);
+      }
+    }
+  }
+}

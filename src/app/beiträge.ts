3dbd5e332a3a3fccb3cb0ee852a802ecbee3015a
theosABS <ferien@apartmomente.ch>
2024-09-27 14:41:25 +0200

    Änderungen

--- src/app/beiträge.ts
+++ src/app/beiträge.ts
@@ -52,19 +52,6 @@ export const Beiträge = {
   
 
 
-     'Der Gemmipass – Ein Weg durch die Jahrhunderte': {
-    karusselltitel: 'Der Gemmipass – Ein Weg durch die Jahrhunderte',
-    verzeichnispfad: 'gemmipass',
-    beitragtitel: 'Der Gemmipass – Ein Weg durch die Jahrhunderte',
-    ort: 'Leukerbad (VS)',
-    bild: 'leukerbad-gemmipass/bild1.jpeg',
-    autor: 'Miriam Löhr',
-    jahr: '2024',
-    markierungen: [[46.39631, 7.61142]],
-    kanton: Kantone.Wallis,
-  },
-
-
  
 
 
